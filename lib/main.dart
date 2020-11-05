804f4716464b69e6346208e11362ed22fd7826cc
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2020-11-05 13:16:04 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: Text(
+        'Hello, Flutter World!!',
+        style: TextStyle(fontSize: 32.0),
+      ),
+    );
+  }
+}
