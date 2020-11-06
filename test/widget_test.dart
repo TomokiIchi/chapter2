a8dfb0936b1728f18ea104d909eac8a779d8ec53
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2020-11-06 20:54:14 +0900

    test second implement completed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,13 @@
 import 'package:chapter2/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('flutter_app sample test!', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
+    await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
   });
 }
